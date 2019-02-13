a6c2fa8031f5b5dd7b2573f574066140242fc6eb
OndrejMelichar <42346135+OndrejMelichar@users.noreply.github.com>
2019-02-13 19:51:18 +0100

    návrh 3 základních entit

---
+++ Entities/Mark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Entities
+{
+    public class Mark
+    {
+        [PrimaryKey, AutoIncrement]
+        public int MarkId { get; set; }
+        public float Value { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ Entities/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Entities
+{
+    public class Subject
+    {
+        [PrimaryKey, AutoIncrement]
+        public int SubjectId { get; set; }
+        public string Name { get; set; }
+    }
+}
