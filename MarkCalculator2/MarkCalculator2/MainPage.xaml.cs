6015775e9d759f7993b4d7e062b56cb173fe83db
OndrejMelichar <42346135+OndrejMelichar@users.noreply.github.com>
2019-02-13 18:32:37 +0100

    Přidejte soubory projektu.

---
+++ MarkCalculator2/MarkCalculator2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MarkCalculator2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
